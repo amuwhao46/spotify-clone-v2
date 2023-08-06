6788741c5c6d2ff343ba1ba64c5b1a8548801f7c
Oke Amuwha <amuwhao46@gmail.com>
2023-08-05 22:14:45 -0400

    Added dynamic routes for the sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,13 +1,29 @@
 "use client";
-import React from 'react'
+import React, { useMemo } from 'react'
 import { usePathname } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 interface sidebarProps {
   children: React.ReactNode
 }
 
 function sidebar ({children}: sidebarProps) {
-  const pathName = usePathname();
+  const pathname = usePathname();
+  const routes = useMemo(() => [
+    {
+      icon: HiHome,
+      label: "Home",
+      active: pathname !== "/search",
+      href: "/",
+    },
+    {
+      icon: BiSearch,
+      label: "Search",
+      active: pathname !== "/search",
+      href: "/search",
+    }
+  ], [pathname])
   return (
     <div>{children}</div>
   )
