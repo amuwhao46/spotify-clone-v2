7020818cf4e8296ce633a157c52f264ae193987a
Oke Amuwha <amuwhao46@gmail.com>
2023-09-02 16:59:38 -0400

    Added history nav buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { useRouter } from "next/navigation";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 
-function Header({}: Props) {
-  return (
-    <div>Header</div>
-  )
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
 }
 
-export default Header
+function Header({ children, className }: HeaderProps) {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Handles logging out
+  };
+
+  return (
+    <div
+      className={twMerge(
+        "h-fit bg-gradient-to-b from-emerald-800 p-6",
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full flex bg-black items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full flex bg-black items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
